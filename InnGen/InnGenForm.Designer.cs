5b31ffb307b39891358a59c8170f2fc80c151650
konotopsky <konotopsky@moedelo.org>
2017-07-13 14:36:02 +0400

    Init commit

---
+++ InnGen/InnGenForm.Designer.cs
@@ -0,0 +1,103 @@
+﻿namespace InnGen
+{
+    partial class InnGenForm
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(InnGenForm));
+            this.tbResult = new System.Windows.Forms.TextBox();
+            this.btnGen = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.notifyIcon = new System.Windows.Forms.NotifyIcon(this.components);
+            this.SuspendLayout();
+            // 
+            // tbResult
+            // 
+            this.tbResult.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.tbResult.Location = new System.Drawing.Point(12, 12);
+            this.tbResult.Name = "tbResult";
+            this.tbResult.ReadOnly = true;
+            this.tbResult.Size = new System.Drawing.Size(287, 20);
+            this.tbResult.TabIndex = 2;
+            this.tbResult.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // btnGen
+            // 
+            this.btnGen.Location = new System.Drawing.Point(305, 11);
+            this.btnGen.Name = "btnGen";
+            this.btnGen.Size = new System.Drawing.Size(43, 20);
+            this.btnGen.TabIndex = 3;
+            this.btnGen.Text = "<<<";
+            this.btnGen.UseVisualStyleBackColor = true;
+            this.btnGen.Click += new System.EventHandler(this.btnGen_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(71, 57);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Mail";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // notifyIcon
+            // 
+            this.notifyIcon.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon.Icon")));
+            this.notifyIcon.Text = "Генератор ИНН";
+            this.notifyIcon.Visible = true;
+            this.notifyIcon.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.notifyIcon_MouseDoubleClick);
+            // 
+            // InnGenForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(360, 44);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btnGen);
+            this.Controls.Add(this.tbResult);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "InnGenForm";
+            this.Text = "Генератор ИНН для юр.лиц";
+            this.Load += new System.EventHandler(this.InnGenForm_Load);
+            this.Move += new System.EventHandler(this.InnGenForm_Move);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox tbResult;
+        private System.Windows.Forms.Button btnGen;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.NotifyIcon notifyIcon;
+    }
+}
+
---
+++ InnGen/InnGenForm.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace InnGen
+{
+    public partial class InnGenForm : Form
+    {
+
+        KeyboardHook hook = new KeyboardHook();
+
+        public InnGenForm()
+        {
+            InitializeComponent();
+
+
+           hook.KeyPressed +=
+           new EventHandler<KeyPressedEventArgs>(hook_KeyPressed);
+            // register the control + alt + F12 combination as hot key.
+            hook.RegisterHotKey(InnGen.ModifierKeys.Control | InnGen.ModifierKeys.Alt,
+                Keys.I);
+        }
+
+        void hook_KeyPressed(object sender, KeyPressedEventArgs e)
+        {
+            tbResult.Text = getInn();
+        }
+
+        private void btnGen_Click(object sender, EventArgs e)
+        {
+            tbResult.Text = getInn();
+        }
+
+        private String getInn()
+        {
+            UInt32[] x = { 2, 4, 10, 3, 5, 9, 4, 6, 8 };
+
+            UInt32 uBase = (UInt32)(new Random().Next(960000000, 969999999));
+            String strRes = uBase.ToString();
+
+            UInt32 Result = 0;
+            for (int i = 8; i >= 0; i--)
+            {
+                Result += (uBase % 10) * x[i];
+                uBase /= 10;
+            }
+
+            strRes += ((Result % 11) % 10).ToString();
+            Clipboard.SetText(strRes);
+
+            return strRes;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            SendMail("smtp.yandex.ru", "mymail@gmail.com", "5IkENZW92v5dogP0GBcrP4Uh", "invite_edo@moedelo.org", "Тема письма", "Тело письма", null);
+        }
+
+        public static void SendMail(string smtpServer, string from, string password, string mailto, string caption, string message, string attachFile = null)
+        {
+            using (MailMessage mm = new MailMessage("konotopsky@moedelo.org", "invite_edo@moedelo.org"))
+            {
+                mm.Subject = "Гномин_тестер_ЭП, 9637004553";
+                mm.Body = "Добрый день.\n\nНастройка завершена.\n\nРоуминг 13\n7714698320\n123456789\n2BM-6662089308-666201001-201412180949241494095";
+                mm.IsBodyHtml = false;
+                using (SmtpClient sc = new SmtpClient("smtp.yandex.ru", 25))
+                {
+                    sc.EnableSsl = true;
+                    sc.DeliveryMethod = SmtpDeliveryMethod.Network;
+                    sc.UseDefaultCredentials = false;
+                    sc.Credentials = new NetworkCredential("konotopsky@moedelo.org", "PCNKB34520");
+                    sc.Send(mm);
+                }
+            }
+        }
+
+        private void InnGenForm_Move(object sender, EventArgs e)
+        {
+            if (this.WindowState == FormWindowState.Minimized)
+            {
+                this.Hide();
+                notifyIcon.ShowBalloonTip(1000, "Генератор ИНН", "При зажатии на Alt + Ctrl + I будет сгенерирован и помещен в буфер обмена ИНН для физ. лица",ToolTipIcon.Info);
+            }
+        }
+
+        private void notifyIcon_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            this.Show();
+            this.WindowState = FormWindowState.Normal;
+        }
+
+        private void InnGenForm_Load(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Minimized;
+        }
+    }
+}
